8b3a0dac8ac3ec9ce3e1df0b74d55330d557f2db
Mobinur Rahman Titu <mobinurrahmantitu@gmail.com>
2025-09-13 15:01:34 +0600

    Add files via upload

---
+++ alphabets/src/main/java/com/mycompany/alphabets/Alphabet.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.alphabets;
+
+public class Alphabet {
+    public static void main(String[] args) {
+        char ch = 'a';
+        while (ch <= 'z') {
+            System.out.print(ch + " ");
+            ch++;
+        }
+    }
+}
+
+    
